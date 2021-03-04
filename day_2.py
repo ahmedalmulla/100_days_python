fefabc456222469a5aa25028fbfc78669d64a09f
ahmedalmulla <46624390+ahmedalmulla@users.noreply.github.com>
2021-03-04 16:17:36 +0300

    Add files via upload

---
+++ day_2.py
@@ -0,0 +1,6 @@
+#first exerice is to get a two digit number and add the two numbers and print the result
+num = input("enter the two digit number")
+first_num = num[0]
+second_num = num[1]
+sum = int(first_num) + int(second_num)
+print (first_num +" + "+ second_num +"=" + str(sum)) 
