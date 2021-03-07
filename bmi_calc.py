9871f5da574644427f09b63690285ce90e844162
ahmedalmulla <46624390+ahmedalmulla@users.noreply.github.com>
2021-03-07 13:50:16 +0300

    Add files via upload

---
+++ bmi_calc.py
@@ -0,0 +1,7 @@
+##BMI calculator 
+## Body Mass index = wight * (hight **)
+
+weight = input("Weight:")
+hight  = input("Hight:")
+bmi = float(weight ) / (float(hight) * float(hight))
+print(int(bmi))
